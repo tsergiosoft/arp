5b135cb783584b00de27cf9fdd6e754cc18c9ad4
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-06 10:37:39 -0700

    commit

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame
+pip3 install PyYAML mavproxy --user
+echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
+
 curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join 1d71939404a9b1e4
 ####   Raspberry Pi   ############################
