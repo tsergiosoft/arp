87acdc0e00ff382800acfe8cf2e2c16348799ddd
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-06 09:51:21 -0700

    commit

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#on GitHub create repo tsergiosoft/arp.git
+#git clone git@github.com:tsergiosoft/arp.git
+#git config alias.acp '! git add . && git commit -a -m "commit" && git push'
+#	Usage!!!!: git acp
+
+#echo "# piard" >> README.md
+#git init
+#git config core.filemode true
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin git@github.com:tsergiosoft/piard.git
+#git push -u origin main
+
+#git fetch
+
+# On Ubuntu - create ssh
+# cd ~/.ssh
+#ssh-keygen -t ed25519 -C "sergtarasenko76@gmail.com"
+
+
+# adduser --system --ingroup video mjpg_streamer
+mkdir "./mydir"
+# python -m pip install pyserial
+
+# Change to the package directory
+cd "$(dirname "${BASH_SOURCE[0]}")"
+# Execute individual configuration scripts
+#./scripts/script1.sh
+#./scripts/script2.sh
+
