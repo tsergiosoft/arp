4e590bcf0d8b7714d226aee5415f4edb26b2e586
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-06 10:25:17 -0700

    commit

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 1d71939404a9b1e4
+####   Raspberry Pi   ############################
+#$ git clone https://github.com/tsergiosoft/arp.git
+#$ cd arp
+#$ sudo ./install.sh
+# git pull origin main
 
+#git ls-tree -r HEAD
+
+################################
 #on GitHub create repo tsergiosoft/arp.git
-#git clone git@github.com:tsergiosoft/arp.git
-#git config alias.acp '! git add . && git commit -a -m "commit" && git push'
+#$ git clone git@github.com:tsergiosoft/arp.git
+#$ git config alias.acp '! git add . && git commit -a -m "commit" && git push'
 #	Usage!!!!: git acp
 
 #echo "# piard" >> README.md
@@ -15,7 +24,8 @@ curl -s https://install.zerotier.com | sudo bash
 #git remote add origin git@github.com:tsergiosoft/piard.git
 #git push -u origin main
 
-#git fetch
+#git update-index --chmod=+x install.sh
+
 
 # On Ubuntu - create ssh
 # cd ~/.ssh
