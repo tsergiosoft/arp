f338d66ab53c9c49e072fdd1f4d50f5b3c4da4e6
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-06 11:03:05 -0700

    commit

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo python3 mav_rc_local.py
 sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame -y
 pip3 install PyYAML mavproxy --user
 echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
