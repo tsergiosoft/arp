e4a70e23db5c877c488681cb3dc6bdd6ed0072ad
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-07 06:10:34 -0700

    commit

--- install.sh
+++ install.sh
@@ -19,10 +19,10 @@ sudo apt-get remove modemmanager -y
 
 
 sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame -y
-sudo python3 -m pip install mavproxy
+sudo pip3 install PyYAML mavproxy --user
+#udo python3 -m pip install mavproxy
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 
-#pip3 install PyYAML mavproxy --user
 #echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
  
 curl -s https://install.zerotier.com | sudo bash
