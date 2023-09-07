13ef0bebc46424653e96a0a2f7636e072fd5bab8
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-06 23:39:52 -0700

    commit

--- mavrun.sh
+++ mavrun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+md folder1
 export LOCALAPPDATA="LOCALAPPDATA"
 screen -L -Logfile mavproxy.log -S mavproxy -d -m bash -c "mavproxy.py --master=/dev/serial0 --force-connected --baudrate 921600 --out=udp:10.243.0.1:14550 --daemon"
 
