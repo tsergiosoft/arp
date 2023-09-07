4a700ff3716363c08f6a6f23159258ed78e86cbb
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-07 06:08:52 -0700

    commit

--- install.sh
+++ install.sh
@@ -17,10 +17,11 @@ sed -i -e '$ascreen -list' /home/pi/.bashrc
 sudo apt-get install screen -y
 sudo apt-get remove modemmanager -y
 
-sudo python3 -m pip install --upgrade mavproxy
+
+sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame -y
+sudo python3 -m pip install mavproxy
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 
-#sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame -y
 #pip3 install PyYAML mavproxy --user
 #echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
  
