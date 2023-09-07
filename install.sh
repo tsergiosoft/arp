95d04c960e8c753ef2495c29ef0bafac1c20cbeb
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-07 09:03:32 -0700

    commit

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-#put line to /etc/rc.local for autorun: 
-#sudo sed -i "\$i sh ~/arp/mavrun.sh &" /etc/rc.local
-#sudo chmod +x /etc/rc.local
-#sudo systemctl enable rc-local.service
-#sudo nano /etc/rc.local
+echo "----------Install screen, Remove modemmanager"
 
+sudo apt-get install screen -y
+sudo apt-get remove modemmanager -y
+echo "----------Install MAVProxy"
+
+sudo pip install MAVProxy
+
+echo "----------Install ZeroTier"
+curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 1d71939404a9b1e4
+
+echo "----------Create service mav"
 #https://github.com/mustafa-gokce/ardupilot-software-development/blob/main/mavproxy/automated-forwarding-services.md
 sudo cp /home/pi/arp/mav.service /etc/systemd/system
 sudo systemctl enable mav.service
@@ -14,9 +21,11 @@ sudo systemctl start mav.service
 #sed -i -e '$ascreen -L -Logfile mavproxy.log -S mavproxy -d -m bash -c "mavproxy.py --master=/dev/serial0 --force-connected --baudrate 921600 --out=udp:10.243.0.1:14550 --daemon"' /home/pi/.bashrc
 #sed -i -e '$ascreen -list' /home/pi/.bashrc
 
-sudo apt-get install screen -y
-sudo apt-get remove modemmanager -y
-sudo pip install MAVProxy
+#put line to /etc/rc.local for autorun: 
+#sudo sed -i "\$i sh ~/arp/mavrun.sh &" /etc/rc.local
+#sudo chmod +x /etc/rc.local
+#sudo systemctl enable rc-local.service
+#sudo nano /etc/rc.local
 
 #sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame -y
 #sudo pip3 install PyYAML mavproxy --user
@@ -25,8 +34,7 @@ sudo pip install MAVProxy
 
 #echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
  
-curl -s https://install.zerotier.com | sudo bash
-sudo zerotier-cli join 1d71939404a9b1e4
+
 
 ####   Raspberry Pi   ############################
 #$ git clone https://github.com/tsergiosoft/arp.git
@@ -58,13 +66,4 @@ sudo zerotier-cli join 1d71939404a9b1e4
 # cd ~/.ssh
 #ssh-keygen -t ed25519 -C "sergtarasenko76@gmail.com"
 
-
-# adduser --system --ingroup video mjpg_streamer
-#mkdir "./mydir"
-# python -m pip install pyserial
-
-# Change to the package directory
-# Execute individual configuration scripts
-#./scripts/script1.sh
-#./scripts/script2.sh
 
--- mavrun.sh
+++ mavrun.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#cd /home/pi/arp
-#mkdir as
 #export LOCALAPPDATA="LOCALAPPDATA"
 screen -L -Logfile mavproxy.log -S mavproxy -d -m bash -c "mavproxy.py --master=/dev/serial0 --force-connected --baudrate 921600 --out=udp:10.243.0.1:14550 --daemon"
-#mavproxy.py --master=/dev/serial0 --force-connected --baudrate 921600 --out=udp:10.243.0.1:14550
-while true ; do echo "loop"; done
+while true ; do sleep 120; done
 
