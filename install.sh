fb66438642622bdd9fb059935b4b6bae0e4703bf
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-07 05:45:17 -0700

    commit

--- install.sh
+++ install.sh
@@ -4,15 +4,11 @@
 #sudo chmod +x /etc/rc.local
 #sudo systemctl enable rc-local.service
 #sudo nano /etc/rc.local
-# journalctl -b
-# ps -fA | grep mavproxy.py
-
 
 #https://github.com/mustafa-gokce/ardupilot-software-development/blob/main/mavproxy/automated-forwarding-services.md
 #sudo cp /home/pi/arp/mav.service /etc/systemd/system
 #sudo systemctl enable mav.service
 #sudo systemctl start myscript.service
-
 
 #sed -i -e '$aexport LOCALAPPDATA="LOCALAPPDATA"' /home/pi/.bashrc
 sed -i -e '$ascreen -L -Logfile mavproxy.log -S mavproxy -d -m bash -c "mavproxy.py --master=/dev/serial0 --force-connected --baudrate 921600 --out=udp:10.243.0.1:14550 --daemon"' /home/pi/.bashrc
@@ -28,17 +24,15 @@ echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 #pip3 install PyYAML mavproxy --user
 #echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
  
-#curl -s https://install.zerotier.com | sudo bash
-#sudo zerotier-cli join 1d71939404a9b1e4
-
+curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 1d71939404a9b1e4
 
 ####   Raspberry Pi   ############################
 #$ git clone https://github.com/tsergiosoft/arp.git
 #$ cd arp
-#$ sudo ./install.sh
+#$ ./install.sh
 # git pull origin main
 
-#git ls-tree -r HEAD
 #sudo ip link set wlan0 down
 #
 ################################
