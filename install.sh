8a0c3d5ca680ec9e6a8b121e2026179054a54f1a
tsergiosoft <sergtarasenko76@gmail.com>
2023-09-07 02:17:12 -0700

    commit

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 
 
 #https://github.com/mustafa-gokce/ardupilot-software-development/blob/main/mavproxy/automated-forwarding-services.md
-cp /home/pi/arp/mav.service /etc/systemd/system
+sudo cp /home/pi/arp/mav.service /etc/systemd/system
 sudo apt-get install screen -y
 sudo apt-get remove modemmanager -y
 sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame -y
