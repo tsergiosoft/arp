377065e0fd884041e55f2ab9de83e12a925c5197
tsergisoft <100694386+tsergiosoft@users.noreply.github.com>
2023-09-10 08:14:33 +0300

    Update cam_run.sh

--- cam_run.sh
+++ cam_run.sh
@@ -3,4 +3,4 @@
 #mjpg_streamer -i '/usr/local/lib/mjpg-streamer/input_uvc.so -d /dev/video0  -f 15 -y -n' -o '/usr/local/lib/mjpg-streamer/output_http.so -w ./www -p 8080'
 #export LD_LIBRARY_PATH=~/mjpg-streamer/mjpg-streamer-experimental
 
-mjpg_streamer -i '/usr/local/lib/mjpg-streamer/input_file.so -e -f /usr/local/bin' -o '/usr/local/lib/mjpg-streamer/output_http.so -w /home/s/mjpg-streamer/mjpg-streamer-experimental/www -p 8080'
+mjpg_streamer -i '/usr/local/lib/mjpg-streamer/input_file.so -e -f /home/pi/arp' -o '/usr/local/lib/mjpg-streamer/output_http.so -w /home/s/mjpg-streamer/mjpg-streamer-experimental/www -p 8080'
