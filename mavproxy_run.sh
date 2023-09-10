9e5a743c9ad40f15bda3d5bc930c0634c527a5dd
tsergisoft <100694386+tsergiosoft@users.noreply.github.com>
2023-09-10 20:47:51 +0300

    Update mavproxy_run.sh

--- mavproxy_run.sh
+++ mavproxy_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LOCALAPPDATA="LOCALAPPDATA"
-mavproxy.py --master=/dev/serial0 --force-connected --baudrate 921600 --out=udp:10.243.0.1:14550
+mavproxy.py --master=/dev/serial0 --force-connected --baudrate 921600 --out=udp:10.243.0.1:14550 --out=tcpin:127.0.0.1:14550
