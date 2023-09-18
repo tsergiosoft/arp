99a55142b280195f592941b9fbc1c95de7ed69d3
tsergisoft <100694386+tsergiosoft@users.noreply.github.com>
2023-09-18 09:05:57 +0300

    Update cam_run.sh

--- cam_run.sh
+++ cam_run.sh
@@ -1,7 +1,10 @@
 #cd ~/mjpg-streamer/mjpg-streamer-experimental
 #export LD_LIBRARY_PATH=.
 
-mjpg_streamer -i '/usr/local/lib/mjpg-streamer/input_uvc.so -d /dev/video0  -f 2 -n' -o '/usr/local/lib/mjpg-streamer/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer-experimental/www -p 8080'
+mjpg_streamer -i '/usr/local/lib/mjpg-streamer/input_uvc.so -d /dev/video0 -n' -o '/usr/local/lib/mjpg-streamer/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer-experimental/www -p 8080'
+
+#YUVU format (f param) - better pic slower stream
+#mjpg_streamer -i '/usr/local/lib/mjpg-streamer/input_uvc.so -d /dev/video0  -f 2 -n' -o '/usr/local/lib/mjpg-streamer/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer-experimental/www -p 8080'
 
 #   file simulation without CAM
 #mjpg_streamer -i '/usr/local/lib/mjpg-streamer/input_file.so -e -f /home/pi/awm/camsimul' -o '/usr/local/lib/mjpg-streamer/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer-experimental/www -p 8080'
